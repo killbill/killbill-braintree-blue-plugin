b23c159011666718aef6f72ea4544ce09c21f651
Pierre-Alexandre Meyer <pierre@mouraf.org>
2016-07-19 22:33:25 -0700

    payment_method: add support for tokens representing a PayPal token
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/braintree_blue/ext/active_merchant/active_merchant.rb
+++ lib/braintree_blue/ext/active_merchant/active_merchant.rb
@@ -2,6 +2,55 @@ module ActiveMerchant
   module Billing
     class BraintreeBlueGateway
 
+      # Add support for other payment methods
+      def add_customer_with_credit_card(creditcard, options)
+        commit do
+          if options[:payment_method_nonce]
+            credit_card_params = { payment_method_nonce: options[:payment_method_nonce] }
+          else
+            credit_card_params = {
+                :credit_card => {
+                    :cardholder_name => creditcard.name,
+                    :number => creditcard.number,
+                    :cvv => creditcard.verification_value,
+                    :expiration_month => creditcard.month.to_s.rjust(2, "0"),
+                    :expiration_year => creditcard.year.to_s,
+                    :token => options[:credit_card_token]
+                }
+            }
+          end
+          parameters = {
+              :first_name => creditcard.first_name,
+              :last_name => creditcard.last_name,
+              :email => scrub_email(options[:email]),
+              :id => options[:customer],
+          }.merge credit_card_params
+          result = @braintree_gateway.customer.create(merge_credit_card_options(parameters, options))
+
+          card_token = nil
+          if result.success? && !result.customer.credit_cards.nil? && !result.customer.credit_cards[0].nil?
+            card_token = result.customer.credit_cards[0].token
+          end
+
+          paypal_token = nil
+          if result.success? && !result.customer.paypal_accounts.nil? && !result.customer.paypal_accounts[0].nil?
+            paypal_token = result.customer.paypal_accounts[0].token
+          end
+
+          Response.new(result.success?, message_from_result(result),
+                       {
+                           :braintree_customer => (customer_hash(result.customer, :include_credit_cards) if result.success?),
+                           :customer_vault_id => (result.customer.id if result.success?),
+                           :credit_card_token => card_token,
+                           :paypal_token => paypal_token,
+                           # Required for BraintreeBluePaymentMethod.from_response
+                           :token => card_token || paypal_token
+                       },
+                       :authorization => (result.customer.id if result.success?)
+          )
+        end
+      end
+
       def add_credit_card_to_customer(credit_card, options)
         commit do
           if options[:payment_method_nonce]
@@ -24,12 +73,20 @@ module ActiveMerchant
           parameters[:billing_address] = map_address(options[:billing_address]) unless options[:billing_address].empty?
 
           result = @braintree_gateway.credit_card.create(parameters)
+
+          card_token = nil
+          if result.success?
+            card_token = result.credit_card.token
+          end
+
           ActiveMerchant::Billing::Response.new(
               result.success?,
               message_from_result(result),
               {
                   customer_vault_id: (result.credit_card.customer_id if result.success?),
-                  credit_card_token: (result.credit_card.token if result.success?)
+                  credit_card_token: card_token,
+                  # Required for BraintreeBluePaymentMethod.from_response
+                  token: card_token
               },
               authorization: (result.credit_card.customer_id if result.success?)
           )
--- lib/braintree_blue/models/payment_method.rb
+++ lib/braintree_blue/models/payment_method.rb
@@ -13,7 +13,8 @@ module Killbill #:nodoc:
         # Unfortunately, the ActiveMerchant Braintree implementation will drop that information when adding a card to an existing customer
         customer_response = primary_response.params['braintree_customer'] || {}
 
-        token = options[:token] || primary_response.params['credit_card_token']
+        # See active_merchant.rb
+        token = options[:token] || primary_response.params['token']
         card_response = (customer_response['credit_cards'] || []).first || {}
         cc_exp_dates = (card_response['expiration_date'] || '').split('/')
 
